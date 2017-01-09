011cdb7a29091a0dd22b317f3a17149f01f20872
Nam Nguyen <nam.nguyen.code@gmail.com>
2017-01-09 17:01:19 +0700

    YES

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,6 +1,30 @@
-self.addEventListener('install', function (e) {
-  console.log('Install Event:', e);
+var version = 1;
+var cacheName = 'stale- ' + version;
+self.addEventListener('install', function(event) {
+self.skipWaiting();
 });
-self.addEventListener('activate', function (e) {
-  console.log('Activate Event:', e);
+self.addEventListener('activate', function(event) {
+if (self.clients && clients.claim) {
+clients.claim();
+}
+});
+self.addEventListener('fetch', function(event) {
+event.respondWith(
+fetch(event.request).then(function(response) {
+caches.open(cacheName).then(function(cache) {
+if(response.status >= 500) {
+cache.match(event.request).
+then(function(response) {
+return response;
+}).catch(function() {
+return response;
+});
+} else {
+cache.put(event.request,
+response.clone());
+return response;
+}
+});
+})
+);
 });
